e081339ac4d5247a70deb362150c3faabb4c84ff
jai-michael <jgrant27@student.scad.edu>
2025-02-25 08:31:19 -0500

    Emotions Class JS, CSS updates, and SVG Renaming

--- js/script.js
+++ js/script.js
@@ -6,32 +6,4 @@ class Emotion {
         this.phrase = `I'm feeling ${title}!`;
     }
 }
-let happyEmotion = {
-    title: "Happy",
-    color: "--happy-color",
-    icon: "happy_face.svg",
-};
-
-let sadEmotion = {
-    title: "Sad",
-    color: "--sad-color",
-    icon: "sad_face.svg",
-};
-
-let afraidEmotion = {
-    title: "Afraid",
-    color: "--afraid-color",
-    icon: "afraid_face.svg",
-};
-
-let angryEmotion = {
-    title: "Angry",
-    color: "--angry-color",
-    icon: "angry_face.svg",
-};
-
-let disgustedEmotion = {
-    title: "Disgusted",
-    color: "--disgusted-color",
-    icon: "disgusted_face.svg",
-};
+let happyEmotion = new Emotion("Happy");
