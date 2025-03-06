67d160000db3f74c897480db1f00fc3066e5d723
jai-michael <jgrant27@student.scad.edu>
2025-03-05 21:48:55 -0500

    Emotion Selector JS Logic

--- js/script.js
+++ js/script.js
@@ -2,41 +2,51 @@ class Emotion {
     constructor(title) {
         this.title = title;
         this.cssColor = `--${title.toLowerCase()}-color`;
-        this.svgIcon = `${title}_face.svg`;
+        this.svgIcon = `face_${title}.svg`;
         this.phrase = `I'm feeling ${title}!`;
     }
 }
-let happyEmotion = new Emotion("Happy");
-let sadEmotion = new Emotion("Sad");
-let angryEmotion = new Emotion("Angry");
-let disgustEmotion = new Emotion("Disgust");
-let fearEmotion = new Emotion("Afraid");
+const happyEmotion = new Emotion("Happy");
+const sadEmotion = new Emotion("Sad");
+const angryEmotion = new Emotion("Angry");
+const disgustEmotion = new Emotion("Disgust");
+const fearEmotion = new Emotion("Afraid");
 
-let emotions = [happyEmotion, sadEmotion, angryEmotion, disgustEmotion, fearEmotion];
+const emotions = [happyEmotion, sadEmotion, angryEmotion, disgustEmotion, fearEmotion];
 
-let emotionSelector = 0;
-let questionElement = document.querySelector(".question-section");
-let emotionMessageElement = document.querySelector(".emotion-message");
+let emotionIndex = 0;
 
-function switchEmotion(direction) {
-    console.log(emotionSelector);
-    if (direction == "next") {
-        emotionSelector += 1;
-        if (emotionSelector > emotions.length - 1) {
-            emotionSelector = 0;
-        }
-    } else if (direction == "back") {
-        emotionSelector -= 1;
-        if (emotionSelector < 0) {
-            emotionSelector = emotions.length - 1;
-        }
-    }
-    let currentEmotion = emotions[emotionSelector];
+const questionElement = document.querySelector(".question-section");
+const emotionMessageElement = document.querySelector(".emotion-message");
+const emotionIconElement = document.querySelector(".emotion-face");
 
-    questionElement.style.background = `var(${currentEmotion.cssColor})`;
-    emotionMessageElement.innerHTML = currentEmotion.phrase;
+function switchEmotion(target) {
+    // lastElement = emotions.pop();
+    // shiftedEmotions = emotions.unshift(lastElement);
+    // // console.log(shiftedEmotions);
+    // emotionIndex = (emotionIndex + index) % emotions.length;
+    console.log(`emotionIndex: ${emotionIndex}`);
+    emotionIndex += target - emotionIndex;
+
+    changeEmotionHTML(emotions[emotionIndex]);
 }
 
+function nextEmotion(direction) {
+    emotionIndex = Math.abs((emotionIndex + direction) % emotions.length);
+    console.log(emotionIndex);
+    // if (emotionIndex < 0) {
+    //     emotionIndex *= -1;
+    // }
+    changeEmotionHTML(emotions[emotionIndex]);
+}
+
+function changeEmotionHTML(targetEmotion) {
+    questionElement.style.background = `var(${targetEmotion.cssColor})`;
+    emotionMessageElement.innerHTML = targetEmotion.phrase;
+    emotionIconElement.src = `../assets/icons/faces/${targetEmotion.svgIcon}`;
+}
+
+/////////// JOURNAL LOGIC //////////////
 function storeJournal(element) {
     element = document.querySelector(element);
     console.log(element);
@@ -50,4 +60,10 @@ function readStorage(data) {
     return storedData;
 }
 
-
+// Journal Character counter
+journalTextArea = document.querySelector("#journal-textbox");
+jounralWordCount = document.querySelector("#character-amount");
+journalTextArea.addEventListener("input", event => {
+    const wordCount = journalTextArea.value.trim().split("").length;
+    jounralWordCount.innerHTML = wordCount;
+});
